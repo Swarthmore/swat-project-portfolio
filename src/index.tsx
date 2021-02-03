4287c2c9426f62bf025ef747f8e01b01f2712b20
noot <devnoot@gmail.com>
2021-02-03 14:20:19 -0500

    First commit Set up webpack Set up babelrc Set up typescript Set up react app boilerplate

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import App from "./App";
+import "./global.css";
+
+const node = document.getElementById("app");
+ReactDOM.render(<App name="Swarthmore" />, node);
