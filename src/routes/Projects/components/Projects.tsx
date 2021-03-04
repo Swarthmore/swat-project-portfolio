8a0f4b4140a67f6c50571c0a853d5e76b222cfd1
Anthony Weed <aweed1@swarthmore.edu>
2021-03-04 18:38:47 -0500

    fixed how team names are handeled on projects page

--- src/routes/Projects/components/Projects.tsx
+++ src/routes/Projects/components/Projects.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import styles from "./styles";
 import { useFirestoreConnect, populate, isLoaded, isEmpty } from "react-redux-firebase";
 import { useSelector } from "react-redux";
@@ -9,32 +9,38 @@ import { Project } from "../../../types/index";
 
 export default function Projects() {
 
-    const [title, setTitle] = React.useState("Projects");
-
     const classes = styles();
 
     const params: { teamId: string } = useParams();
     
     const populates = [{ child: "meta.createdBy", root: "users" }]; 
-    const collection = "projects";
 
     useFirestoreConnect([
         { 
-            collection,
+            collection: "projects",
             populates
+        },
+        {
+            collection: "teams"
         } 
     ]);
 
     let projects = useSelector((state:any) => populate(state.firestore, "projects", populates));
+    const teams = useSelector((state: any) => state.firestore.ordered.teams);
 
     // Show message while projects are loading
-    if (!isLoaded(projects)) {
+    if (!isLoaded(projects) || !isLoaded(teams)) {
         return <div>Loading...</div>
     }
 
     // Show message if there is are no projects 
     if (isEmpty(projects)) {
         return <div>Projects empty</div>
+    }
+
+    // Show message if there are no teams (this shouldn't happen)
+    if (isEmpty(teams)) {
+        return <div>Teams empty</div>
     }
 
     // convert to array
@@ -45,30 +51,16 @@ export default function Projects() {
         }
     }
 
-    // when the team id changes, make sure the page title is correct
-    // TODO: THIS IS BAD CHANGE THIS
-    React.useEffect(() => {
-
-        if (params.teamId === "2Ic0FR9LZ6njQ6xO54co") {
-            setTitle("Academic Technology Projects");
-        } else if (params.teamId === "6nwyDhn3F8chk9uPxfiv") {
-            setTitle("Web Team Projects");
-        } else if (params.teamId === "rmiNR7hL6VSVjJSJe61l") {
-            setTitle("AIS Projects");
-        } else if (params.teamId === "xJd3FTSNpudQ0eX5d8No") {
-            setTitle("Help Desk Projects");
-        } else if (params.teamId === "yigGHL5Xcdu0N8gcCGI0") {
-            setTitle("Networking Projects");
-        } else if (params.teamId === "all") {
-            setTitle("All Projects");
-        }
-
-    }, [params.teamId]);
+    const [team] = teams.filter((team: any) => team.id === params.teamId);
 
     return (
         <div className={classes.root}>
-            <Typography variant="h3">{title}</Typography>
-            {Array.isArray(projects) && projects.map((project: Project) => <div className={classes.row} key={project.id}><ProjectCard project={project} /></div>)}
+            <Typography variant="h3">{team ? team.name : "All"} Projects</Typography>
+            {Array.isArray(projects) && projects.map((project: Project) => (
+                <div className={classes.row} key={project.id}>
+                    <ProjectCard project={project} />
+                </div>
+            ))}
         </div>
     );
 
