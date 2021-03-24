44d9085ae8b31c4b2ec4d2095d8d41b5693e7506
Anthony Weed <aweed1@swarthmore.edu>
2021-03-24 10:45:52 -0400

    Add better styling for single project page

--- src/routes/Project/components/styles.ts
+++ src/routes/Project/components/styles.ts
@@ -2,7 +2,9 @@ import { makeStyles } from "@material-ui/core"
 
 export default makeStyles(theme => ({
     root: {
-        flexGrow: 1
+        flexGrow: 1,
+        paddingLeft: theme.spacing(5),
+        paddingRight: theme.spacing(5)
     },
     divider: {
         marginTop: theme.spacing(2),
@@ -14,5 +16,27 @@ export default makeStyles(theme => ({
     update: {
         marginTop: theme.spacing(1),
         marginBottom: theme.spacing(1)
+    },
+    markdown: {
+        border: "1px dashed #ccc",
+        boxSizing: "border-box",
+        padding: theme.spacing(1),
+        marginTop: theme.spacing(1)
+    },
+    skeleton: {
+        flexGrow: 1,
+        height: 300
+    },
+    title: {
+        marginTop: theme.spacing(5),
+        marginBottom: theme.spacing(5)
+    },
+    subtitle: {
+        marginTop: theme.spacing(5),
+        marginBottom: theme.spacing(5)
+    },
+    section: {
+        marginTop: theme.spacing(5),
+        marginBottom: theme.spacing(5)
     }
 }));
