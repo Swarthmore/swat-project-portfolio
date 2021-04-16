0476025447c8beebd6ebdd3d44f6a32430acaa88
devnoot <devnoot@gmail.com>
2021-04-16 09:56:12 -0400

    Removed localStorage code for dark mode settings

--- src/contexts/DarkMode.tsx
+++ src/contexts/DarkMode.tsx
@@ -1,15 +1,24 @@
 import {createContext, useState} from "react";
 
 // TODO: Get this setting from the browser, then set it as default
-export const DarkModeCtx = createContext<{ isOn: boolean, toggle: () => void}>({
+export const DarkModeCtx = createContext<{isOn: boolean, toggle: () => void}>({
     isOn: false,
     toggle: () => {}
 });
 
-export const DarkModeProvider = ({children}: { children: JSX.Element }) => {
-    const [isOn, setIsOn] = useState(false);
+export const DarkModeProvider = ({children}: {children: JSX.Element}) => {
 
-    const toggle = () => setIsOn(!isOn);
+    // check user's browser preferences for dark mode
+    const matches = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    const [isOn, setIsOn] = useState(matches);
+
+    const toggle = () => {
+        if (isOn) {
+            setIsOn(false);
+        } else {
+            setIsOn(true);
+        }
+    }
 
     return (
         <DarkModeCtx.Provider value={{isOn, toggle}}>{children}</DarkModeCtx.Provider>
