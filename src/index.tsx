cbe559739e6f645892f0175a7837a3873cb334bc
devnoot <devnoot@gmail.com>
2021-04-20 11:44:44 -0400

    Adds version number and name to page title

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,15 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import store from "./store/store";
 import createRoutes from "./routes";
-
+import {version} from "../package.json";
+import {appConfig} from "./config";
 import App from "./containers/App";
 import "./global.css";
 
 const target = document.getElementById("root");
 ReactDOM.render(<App store={store} routes={createRoutes()} />, target);
+
+// change the title
+const title = [appConfig.name, version].join(" ");
+window.document.title = title;
+
